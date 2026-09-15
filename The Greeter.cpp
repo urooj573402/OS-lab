c20cf6788ccfc631b2284ad88fe5ffa0d824355f
Urooj Fatima <fatimauf853@gmail.com>
2026-09-15 08:23:32 -0700

    lab 1

---
+++ The Greeter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // ??? ???? ?? ??? ???? ??? ??? ??? ??
+    if (argc > 1) {
+        std::cout << "Hello, " << argv[1] << "!\n";
+    } else {
+        std::cout << "Hello,Alice!\n";
+    }
+    return 0;
+}
